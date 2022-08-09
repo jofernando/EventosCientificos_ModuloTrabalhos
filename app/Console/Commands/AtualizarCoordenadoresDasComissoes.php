4ecaeaa0300cd7f49b8c96dcf4e6c2a97e862574
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-08-09 01:20:45 -0300

    laravel 7 upgrade

--- app/Console/Commands/AtualizarCoordenadoresDasComissoes.php
+++ app/Console/Commands/AtualizarCoordenadoresDasComissoes.php
@@ -36,7 +36,7 @@ class AtualizarCoordenadoresDasComissoes extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
     public function handle()
     {
