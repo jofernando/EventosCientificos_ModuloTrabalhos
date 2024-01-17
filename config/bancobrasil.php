3c33a6ab640fc1ae59a5867dd743f1794f07cd62
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-01-17 20:25:19 -0300

    configs das variaveis do bb

---
+++ config/bancobrasil.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'client' => [
+        'id' => env('BANCOBRASIL_CLIENT_ID', ''),
+        'secret' => env('BANCOBRASIL_CLIENT_SECRET', ''),
+    ],
+    'chave_pix' => env('BANCOBRASIL_CHAVE_PIX', ''),
+];
